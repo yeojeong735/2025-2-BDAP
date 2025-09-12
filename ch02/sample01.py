5dd023101fc281c90bb385d20c60f817410b5174
yeojeong <yeojeong735@gmail.com>
2025-09-12 15:33:04 +0900

    샘플 파일 수정

--- ch02/sample01.py
+++ ch02/sample01.py
@@ -1,5 +1,11 @@
 a = 10
 print(a)
+print(type(a))
+print(id(a))
+print(dir(a))
 
-b = 20
+b = '20'
 print(b)
+print(type(b))
+print(id(b))
+print(dir(b))
---
+++ ch02/sample03.py
@@ -0,0 +1,20 @@
+from Library.share.arrow.gdb.gdb_arrow import Decimal128
+
+a = 10
+print(a)
+print(type(a))
+
+a = '10'
+print(a)
+print(type(a))
+
+# ------------
+
+a = 0.1
+b = 0.2
+
+c = a + b
+print(c)
+
+# a1 = Decimal128('0.1')
+# b1 = Decimal128('0.2')
