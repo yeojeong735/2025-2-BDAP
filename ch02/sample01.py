9bde6afa5fa83b79ad27fe01941a9c3348851161
yeojeong <yeojeong735@gmail.com>
2025-10-06 18:09:51 +0900

    2주차 실습 복습 (1회차)

--- ch02/sample01.py
+++ ch02/sample01.py
@@ -2,7 +2,7 @@ a = 10
 print(a)
 print(type(a))
 print(id(a))
-print(dir(a))
+print(dir(a)) # : 객체가 가지고 있는 속성과 메서드의 리스트 반환
 
 b = '20'
 print(b)
--- ch02/sample03.py
+++ ch02/sample03.py
@@ -1,20 +1,23 @@
-from Library.share.arrow.gdb.gdb_arrow import Decimal128
 
 a = 10
-print(a)
-print(type(a))
+b = '10'
+#c = a + b <- 오류!
+#print(c)
 
-a = '10'
-print(a)
-print(type(a))
 
-# ------------
+print('-----------')
+
+
+from decimal import Decimal
+# decimal 클래스 : 부동 소수점 오차 문제를 해결하기 위해 고안됨
 
 a = 0.1
 b = 0.2
-
 c = a + b
 print(c)
 
-# a1 = Decimal128('0.1')
-# b1 = Decimal128('0.2')
+a1 = Decimal('0.1')
+b1 = Decimal('0.2')
+c1 = a1 + b1
+print(c1)
+print(type(c1))
