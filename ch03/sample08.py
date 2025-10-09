81370f239821dc4622e68a99626fd9e8c91ffa13
yeojeong <yeojeong735@gmail.com>
2025-10-10 08:59:40 +0900

    3주차 실습 복습 (1회차)

--- ch03/sample08.py
+++ ch03/sample08.py
@@ -1,11 +1,11 @@
 list_value_1 = [1, 2, 3]
 list_value_2 = [4, 5, 6]
-
+'''
 print(id(list_value_1))
 print(id(list_value_2))
 list_value_1.extend(list_value_2)
 print(list_value_1)
-'''
+
 list_value_3 = list_value_1 + list_value_2
 
 
@@ -16,13 +16,13 @@ print(list_value_3)
 
 
 
-'''
-list_value_3 = list_value_1 + list_value_2
+
+list_value_1 = list_value_1 + list_value_2
 
 print(list_value_1)
 print(list_value_2)
 
 print(id(list_value_1))
 print(id(list_value_2))
-'''
+
 
--- ch03/sample09.py
+++ ch03/sample09.py
@@ -1,3 +1,11 @@
 tuple_value = (1, 2, 3)
 list_value = [4, 5, 6]
 
+print(tuple_value)
+print(list_value)
+
+tuple_to_list = tuple(list_value)
+print(tuple_to_list)
+
+list_to_tuple = list(tuple_value)
+print(list_to_tuple)
